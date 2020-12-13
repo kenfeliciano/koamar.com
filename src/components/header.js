9ea7d660f71b2b1b77fb230590cfb2c4421eff09
Ken Feliciano <kafeliciano@gmail.com>
2020-12-12 23:21:43 -0800

    Implement dark mode with Tailwind
    * make the header toggle in dark mode
    * add a little hope to the home page

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ import { DarkToggle } from '../components'
 
 const Header = ({ siteTitle }) => (
   <header className='bg-gray-300 text-gray-900 dark:bg-gray-900 dark:text-gray-200 p-4 flex justify-between items-center'>
-    <h1 className='text-gray-900 dark:text-gray-200 no-underline m-0'>
+    <h1 className='text-primary dark:text-secondary no-underline m-0'>
       <Link to='/'>{siteTitle}</Link>
     </h1>
     <DarkToggle />
