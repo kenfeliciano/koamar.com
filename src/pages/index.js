b5f92e987872dec319cc248a149d9c7be3619dee
Ken Feliciano <kafeliciano@gmail.com>
2020-12-15 17:42:31 -0800

    Make it all look consistent in dark mode (#19)
    Not sure I like how it looks in dark mode. These are some recommended colors
    but it looks so white on black. I've done other examples that are subtler.
    At least it is not jarringly wrong and inconsistent for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,17 +7,19 @@ import { Hero } from '../components'
 const IndexPage = () => (
   <Layout>
     <SEO title='Home' />
-    <h1 className='text-xl2'>Hi all!</h1>
+    <h1 className='text-xl2 mt-4'>Hi all!</h1>
     <Hero />
     <hr />
-    Not much to see yet, but...
-    <h2>Updates so far!</h2>
-    <ul>
-      <li className='list-disc'>
-        Dark Mode Implementation with <a href='https://tailwindcss.com'>Tailwind CSS</a>
-      </li>
-    </ul>
-    <h2>Be well!</h2>
+    <p>Not much to see yet, but...</p>
+    <div className='bg-container rounded-lg shadow-lg p-4'>
+      <h2>Updates so far!</h2>
+      <ul>
+        <li className='list-disc'>
+          Dark Mode Implementation with <a href='https://tailwindcss.com'>Tailwind CSS</a>
+        </li>
+      </ul>
+    </div>
+    <h2 className='text-xl mt-3'>Be well!</h2>
     <p>
       Still, probably best to <a href='https://google.com'>search for someplace</a> better for now.
     </p>
