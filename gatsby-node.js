2ffe1a751e7f6cab313e0b7801ff685b4b465134
Ken Feliciano <kafeliciano@gmail.com>
2020-12-20 00:36:53 -0800

    Create collection node to create correct path
    blog posts are in blog/
    project posts are in project/
    site posts are in site/

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,27 @@
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNode, createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark` || node.internal.type === 'Mdx') {
+    createNodeField({
+      name: `collection`,
+      node,
+      value: getNode(node.parent).sourceInstanceName,
+    })
+  }
+}
+
 exports.createPages = async ({ graphql, actions }) => {
   const { data } = await graphql(`
     query {
       allMdx(sort: { fields: frontmatter___date, order: DESC }) {
         edges {
           node {
+            id
             frontmatter {
               slug
             }
-            id
+            fields {
+              collection
+            }
           }
         }
       }
@@ -18,8 +32,10 @@ exports.createPages = async ({ graphql, actions }) => {
   data.allMdx.edges.forEach((edge) => {
     const slug = edge.node.frontmatter.slug
     const id = edge.node.id
+    const collection = edge.node.fields.collection
+
     actions.createPage({
-      path: slug,
+      path: `${collection}/${slug}`,
       component: require.resolve('./src/templates/blog-post.js'),
       context: { id },
     })
