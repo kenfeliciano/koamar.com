dcc0b94cfce0fc6f4b63dbd8455a3f10271bac46
Ken Feliciano <kafeliciano@gmail.com>
2020-12-21 16:58:57 -0800

    Convert layout div to SiteWrapper tw-styled-component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,11 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
+import tw, { styled } from 'twin.macro'
 
 import { Header, ExternalLink } from '.'
+
+const SiteWrapper = styled.div(() => [tw`h-full m-auto max-w-screen-lg pt-0 px-4 pb-6 mt-6`])
 
 export const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -18,20 +21,14 @@ export const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0rem 1.5rem`,
-        }}
-      >
-        <main className='mt-6'>{children}</main>
+      <SiteWrapper>
+        <main>{children}</main>
         <footer className='text-muted text-sm mt-6'>
           © {new Date().getFullYear()}, Built with
           {` `}
           <ExternalLink href='https://www.gatsbyjs.com'>Gatsby</ExternalLink>
         </footer>
-      </div>
+      </SiteWrapper>
     </>
   )
 }
