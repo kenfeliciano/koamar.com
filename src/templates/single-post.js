a46d3d142ee46d1c8641bb26e57e91809ad660b4
Ken Feliciano <kafeliciano@gmail.com>
2020-12-24 18:26:37 -0800

    Add basic frontmatter to posts (#34)
    Added implementation date, created date, and createdCirca, for approx.

--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -4,14 +4,28 @@ import { MDXRenderer } from 'gatsby-plugin-mdx'
 import SEO from '../components/seo'
 import { Layout, CoverImage, Content } from '../components'
 
+const getThirdField = ({ implementation, created, createdCirca, date }) => {
+  if (implementation) return <span>Implemented: {implementation}</span>
+  if (created) return <span>Written on: {created}</span>
+  if (createdCirca) return <span>Written during: {createdCirca}</span>
+  return <span className='invisible'>Posted: {date}</span>
+}
+
 const BlogPost = ({ data }) => {
-  const coverImage = data.mdx.frontmatter.coverImage ? data.mdx.frontmatter.coverImage.childImageSharp.fluid : null
+  const frontmatter = data.mdx.frontmatter
+  const coverImage = frontmatter.coverImage ? frontmatter.coverImage.childImageSharp.fluid : null
+  const thirdField = getThirdField(frontmatter)
   return (
     <Layout>
-      <SEO title={data.mdx.frontmatter.title} description={data.mdx.frontmatter.excerpt} />
+      <SEO title={frontmatter.title} description={frontmatter.excerpt} />
       <CoverImage fluid={coverImage} />
+      <div className='flex justify-between text-sm text-muted mt-1 ml-4 lg:ml-0 mr-4 lg:mr-0'>
+        <span>Posted: {frontmatter.date}</span>
+        <span>{data.mdx.timeToRead} min. read</span>
+        {thirdField}
+      </div>
       <Content>
-        <h1>{data.mdx.frontmatter.title}</h1>
+        <h1>{frontmatter.title}</h1>
         <MDXRenderer>{data.mdx.body}</MDXRenderer>
       </Content>
     </Layout>
@@ -30,6 +44,9 @@ export const blogQuery = graphql`
         slug
         title
         tags
+        implementation(formatString: "MM/DD/YYYY")
+        created(formatString: "MM/DD/YYYY")
+        createdCirca
         coverImage {
           publicURL
           childImageSharp {
@@ -40,6 +57,9 @@ export const blogQuery = graphql`
         }
       }
       timeToRead
+      fields {
+        collection
+      }
     }
   }
 `
