d8820bb173fbb71d3e3f8cbb5d2b66be1e1cb93d
Ken Feliciano <kafeliciano@gmail.com>
2020-12-29 19:10:53 -0800

    Create paginated landing pages for collections
    * Fun JavaScript in gatsby-node (to me!)
    * Needed to create post-list template
    * Ended up doing more than I had hoped to the post-list template

--- gatsby-node.js
+++ gatsby-node.js
@@ -28,6 +28,35 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
+  // Create paginated pages for posts
+  const postsPerPage = 6
+  const edges = data.allMdx.edges
+  const collectionCount = (arr, val) => arr.reduce((a, edge) => (edge.node.fields.collection === val ? a + 1 : a), 0)
+
+  let groups = []
+
+  groups.push({ groupName: 'blog', count: collectionCount(edges, 'blog') })
+  groups.push({ groupName: 'project', count: collectionCount(edges, 'project') })
+  groups.push({ groupName: 'site', count: collectionCount(edges, 'site') })
+
+  groups.forEach(({ groupName, count }) => {
+    const numPages = Math.ceil(count / postsPerPage)
+    Array.from({ length: numPages }).forEach((_, i) => {
+      actions.createPage({
+        path: i === 0 ? `/${groupName}` : `/${groupName}/${i + 1}`,
+        component: require.resolve('./src/templates/post-list.js'),
+        context: {
+          limit: postsPerPage,
+          skip: i * postsPerPage,
+          numPages,
+          currentPage: i + 1,
+          collection: groupName,
+          coverImage: `${groupName}-cover.jpg`,
+        },
+      })
+    })
+  })
+
   // Create single blog posts
   data.allMdx.edges.forEach((edge) => {
     const slug = edge.node.frontmatter.slug
---
+++ src/templates/post-list.js
@@ -0,0 +1,80 @@
+import * as React from 'react'
+import { graphql, Link } from 'gatsby'
+import Img from 'gatsby-image'
+
+import SEO from '../components/seo'
+import { Layout, CoverImage } from '../components'
+
+const postList = ({ pageContext, data }) => {
+  const { collection } = pageContext
+  const posts = data.allMdx.edges
+
+  return (
+    <Layout>
+      <SEO />
+      <CoverImage />
+      <h1>{collection} posts</h1>
+      <h2>To Do List:</h2>
+      <ul>
+        <li>Add cover image that is correct for the collection</li>
+        <li>Add blurb for what is going to go into this section (short!) This should come from an Mdx file.</li>
+        <li>Create Card components for the posts and format them better</li>
+        <li>Add pagination</li>
+      </ul>
+      {posts.map((post) => (
+        <div key={post.node.id} className='flex rounded-lg shadow-lg bg-surface mt-4 p-4'>
+          {post.node.frontmatter.coverImage ? (
+            <Img
+              className='absolute top-0 left-0 w-36 h-24 flex-none'
+              fluid={post.node.frontmatter.coverImage.childImageSharp.fluid}
+            />
+          ) : (
+            <div className='bg-brandedSurface w-36 h-24'></div>
+          )}
+          <div className='ml-4 relative flex flex-col justify-between'>
+            <div>
+              <Link to={`/${collection}/${post.node.frontmatter.slug}`}>
+                <h2>{post.node.frontmatter.title}</h2>
+              </Link>
+              <p className='mt-1'>{post.node.frontmatter.excerpt}</p>
+            </div>
+            <p className='text-sm text-muted'>{post.node.frontmatter.date}</p>
+          </div>
+        </div>
+      ))}
+    </Layout>
+  )
+}
+
+export default postList
+
+export const pageQuery = graphql`
+  query AllPostsQuery($skip: Int!, $limit: Int!, $collection: String!) {
+    allMdx(
+      sort: { fields: frontmatter___date, order: DESC }
+      filter: { fields: { collection: { eq: $collection } } }
+      skip: $skip
+      limit: $limit
+    ) {
+      edges {
+        node {
+          frontmatter {
+            slug
+            title
+            date(formatString: "MMMM D, YYYY")
+            excerpt
+            coverImage {
+              publicURL
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          id
+        }
+      }
+    }
+  }
+`
