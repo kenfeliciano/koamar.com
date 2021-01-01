064e76c231e881b2a541ebf5706cdfe79592f553
Ken Feliciano <kafeliciano@gmail.com>
2021-01-01 15:30:28 -0800

    Bring over the responsive header
    Update to site colors

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,21 +1,58 @@
 import * as React from 'react'
+import { Image } from '../components'
 
 export const Hero = () => (
-  <div className='bg-surface p-4 rounded-lg shadow-lg mt-4'>
-    <p>
-      Welcome to <span className='text-primary font-semibold font-sans'>KoaMar</span> in 2020, home of Ken Feliciano
-    </p>
-    <p className='text-3xl leading-none tracking-tight font-extrabold sm:text-2xl'>
-      <span className='hidden sm:inline'>Lifelong learner</span>
-      <span className='sm:hidden'>Learner</span>, musician<span className='md:hidden'>, coder</span>
-      <span className='hidden sm:inline'>, retired guy</span>
-      <span className='hidden md:inline'>, some sort of coding type</span>
-    </p>
-    <p className='hidden lg:block text-body'>
-      I hope to be documenting my learning and creative work for future me and anyone else that finds this interesting!
-    </p>
-    <p className='hidden sm:block lg:hidden'>Documenting learning and creating stuff...for the future</p>
-    <p className='sm:hidden'>Documenting learning and creating stuff</p>
-    <p>It's been 24 years, about time something happened!</p>
-  </div>
+  <>
+    <div className='relative h-64 m-0 overflow-hidden rounded-lg bg-brandedSurface dark:bg-opposite'>
+      <div className='absolute z-30 flex w-full h-full items-center'>
+        <div className='relative z-30 w-5/6 px-6 py-8 text-body md:py-6 md:w-1/2'>
+          <div className='hidden md:inline'>
+            <h2 className='text-3xl lg:text-5xl'>
+              <span className='hidden md:inline'>Lifelong learner, </span>
+              <span className='md:hidden'>
+                Learner, <br />
+              </span>
+              musician, <span className='hidden md:inline'>retired guy, </span>
+              <span className='hidden lg:inline'>some sort of coding type</span>
+              <span className='lg:hidden'>coder</span>
+            </h2>
+          </div>
+          <div className='md:hidden'>
+            <h2 className='text-3xl'>
+              <span>Learner</span>
+              <br />
+              <span>Musician</span>
+              <br />
+              <span>Coder</span>
+              <br />
+              <span>Retired Guy</span>
+            </h2>
+          </div>
+          <span></span>
+        </div>
+        <div className='absolute top-0 right-0 flex w-full h-full'>
+          <div className='sm:w-1/3 w-14 h-full bg-brandedSurface dark:bg-opposite transition-all'></div>
+          <div className='relative w-1/3'>
+            <svg
+              fill='currentColor'
+              viewBox='0 0 100 100'
+              className='absolute inset-y-0 z-20 h-full text-brandedSurface dark:text-opposite'
+            >
+              <polygon id='diagonal' points='0,0 100,0 50,100 0,100'></polygon>
+            </svg>
+            <svg
+              fill='currentColor'
+              viewBox='0 0 100 100'
+              className='absolute inset-y-0 z-10 h-full ml-6 text-white opacity-50'
+            >
+              <polygon points='0,0 100,0 50,100 0,100'></polygon>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className='absolute top-0 right-0 block w-8/12 h-full'>
+        <Image className='object-cover min-w-full h-full' alt='Synthesizer - photo by Steve Harvey @ Unsplash' />
+      </div>
+    </div>
+  </>
 )
