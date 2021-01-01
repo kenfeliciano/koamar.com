fbf60b8fcfee797c5ee88818170006adbc529175
Ken Feliciano <kafeliciano@gmail.com>
2021-01-01 01:34:22 -0800

    Tweaking DarkToggle styles for desktop
    Mobile looked great but desktop had issues.
    Spent quite a bit of time trying things before realizing
    the button was set to inline and therefore,
    it didn't care about my top paddings and margins!

--- src/components/DarkToggle.js
+++ src/components/DarkToggle.js
@@ -11,16 +11,16 @@ export const DarkToggle = () => {
   }
 
   return (
-    <div className='hover:bg-opposite'>
+    <div className='hover:bg-opposite sm:hover:bg-container sm:ml-2'>
       <button
         type='button'
         onClick={() => {
           setTheme(theme === 'light' ? 'dark' : 'light')
         }}
-        className='sm:inline sm:ml-2 flex justify-between items-center w-full pr-6'
+        className='sm:block flex justify-between items-center w-full pr-6 sm:pr-0'
       >
         <span className='block sm:hidden px-2 py-1 font-semibold rounded hover:bg-opposite'> Toggle Theme </span>
-        <span className='block'>{`${theme === 'light' ? '🌙' : '🌞'}`}</span>
+        <span className='block sm:inline-block sm:py-1'>{`${theme === 'light' ? '🌙' : '🌞'}`}</span>
       </button>
     </div>
   )
