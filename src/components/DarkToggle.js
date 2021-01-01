3ab7b3a5cedb099e05caa8791d471e9a59b8546d
Ken Feliciano <kafeliciano@gmail.com>
2021-01-01 00:56:30 -0800

    Make dark toggle match in drop-down menu

--- src/components/DarkToggle.js
+++ src/components/DarkToggle.js
@@ -11,14 +11,17 @@ export const DarkToggle = () => {
   }
 
   return (
-    <button
-      type='button'
-      onClick={() => {
-        setTheme(theme === 'light' ? 'dark' : 'light')
-      }}
-      className='block'
-    >
-      {`${theme === 'light' ? '🌙' : '🌞'}`}
-    </button>
+    <div className='hover:bg-opposite'>
+      <button
+        type='button'
+        onClick={() => {
+          setTheme(theme === 'light' ? 'dark' : 'light')
+        }}
+        className='sm:inline sm:ml-2 flex justify-between items-center w-full pr-6'
+      >
+        <span className='block sm:hidden px-2 py-1 font-semibold rounded hover:bg-opposite'> Toggle Theme </span>
+        <span className='block'>{`${theme === 'light' ? '🌙' : '🌞'}`}</span>
+      </button>
+    </div>
   )
 }
