59e4d9b2ae2de893c702eb0994c7b6cadaf43dc2
Ken Feliciano <kafeliciano@gmail.com>
2021-01-02 16:51:06 -0800

    Post card (#42)
    * Extract Post Card and format for mobile first
    
    * Make whole card clickable
    
    At the expensive of selecting the text

--- src/templates/post-list.js
+++ src/templates/post-list.js
@@ -1,10 +1,9 @@
 import * as React from 'react'
-import { graphql, Link } from 'gatsby'
-import Img from 'gatsby-image'
+import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 import SEO from '../components/seo'
-import { Layout, CoverImage, Content } from '../components'
+import { Layout, CoverImage, Content, PostCard } from '../components'
 
 const postList = ({ pageContext, data }) => {
   const { collection } = pageContext
@@ -19,25 +18,7 @@ const postList = ({ pageContext, data }) => {
         <h1>{site.frontmatter.title}</h1>
         <MDXRenderer>{site.body}</MDXRenderer>
         {posts.map((post) => (
-          <div key={post.node.id} className='flex rounded-lg shadow-lg bg-surface mt-4 p-4'>
-            {post.node.frontmatter.coverImage ? (
-              <Img
-                className='absolute top-0 left-0 w-36 h-24 flex-none'
-                fluid={post.node.frontmatter.coverImage.childImageSharp.fluid}
-              />
-            ) : (
-              <div className='bg-brandedSurface w-36 h-24'></div>
-            )}
-            <div className='ml-4 relative flex flex-col justify-between'>
-              <div>
-                <Link to={`/${collection}/${post.node.frontmatter.slug}`}>
-                  <h2>{post.node.frontmatter.title}</h2>
-                </Link>
-                <p className='mt-1'>{post.node.frontmatter.excerpt}</p>
-              </div>
-              <p className='text-sm text-muted'>{post.node.frontmatter.date}</p>
-            </div>
-          </div>
+          <PostCard post={post} collection={collection} />
         ))}
       </Content>
     </Layout>
