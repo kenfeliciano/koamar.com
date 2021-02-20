6b27be788c37d138cc667d202ef2d88e42ba70ad
Ken Feliciano <kafeliciano@gmail.com>
2021-02-19 17:45:20 -0800

    Site external links (#67)
    * Had wrapPageElement instead of Root!
    
    Thought this might be causing blank web site issue with local build
    but it did not fix it.
    Refreshing the home page results in blank. Going to another route works.
    Then going back to the home page works too!
    
    * Add Tailwind styles to border-fadeaway class
    
    Make changes wherever these styled occur.
    
    * Add Site external-links post

--- src/components/ExternalLink.js
+++ src/components/ExternalLink.js
@@ -1,12 +1,7 @@
 import * as React from 'react'
 
 export const ExternalLink = ({ href, children }) => (
-  <a
-    href={href}
-    className='inline-link border-fadeaway border-primary border-b-2'
-    target='_blank'
-    rel='noreferrer noopener nofollow'
-  >
+  <a href={href} className='inline-link border-fadeaway' target='_blank' rel='noreferrer noopener nofollow'>
     {children}
   </a>
 )
--- src/components/InternalLink.js
+++ src/components/InternalLink.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 
 export const InternalLink = ({ href, children }) => (
-  <Link to={href} className='inline-link border-fadeaway border-primary border-b-2'>
+  <Link to={href} className='inline-link border-fadeaway'>
     {children}
   </Link>
 )
