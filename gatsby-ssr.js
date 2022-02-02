bc0d7450c94035b12bbc0f3ba8da3bd785bfe61b
Ken Feliciano <kafeliciano@gmail.com>
2022-02-01 16:01:50 -0800

    Site content wrappers (#76)
    * Update README acknowledgements
    
    Coner Murphy blog post on linked headers in Mdx, in Gatsby.
    
    * Update h2 header linking syntax
    
    It originally searched for a space to replace with a dash.
    It only did this for the first space so anything with more than two
    words did not work. Changed to RegEx (no new bugs introduced!).
    
    * Add Site Content Wrappers Post

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -53,8 +53,8 @@ const components = {
   },
   a: (props) => <MarkdownLink {...props} />,
   h2: (props) => (
-    <h2 id={props.children.replace(' ', '-').toLowerCase()}>
-      <a href={`#${props.children.replace(' ', '-').toLowerCase()}`}>{props.children}</a>
+    <h2 id={props.children.replace(/\s/g, '-').toLowerCase()}>
+      <a href={`#${props.children.replace(/\s/g, '-').toLowerCase()}`}>{props.children}</a>
     </h2>
   ),
   wrapper: ({ children }) => <>{children}</>,
