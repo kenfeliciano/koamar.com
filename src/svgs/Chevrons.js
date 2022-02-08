fc4236ee261925c93b98dd501933c1e6ea05a48d
Ken Feliciano <kafeliciano@gmail.com>
2022-02-08 12:43:19 -0800

    Rollback SVG changes except for Logo (#81)
    I still don't like it but when it settles, it looks correct.
    It shouldn't ***have*** to settle though!

--- src/svgs/Chevrons.js
+++ src/svgs/Chevrons.js
@@ -1,54 +1,45 @@
 import * as React from 'react'
-import styled from 'styled-components'
 
-const ChevronWrapper = styled.div`
-  height: 21px;
-  width: 21px;
-`
 export const DoubleChevronLeft = () => (
-  <ChevronWrapper>
-    <svg
-      className='inline fill-current'
-      height='21'
-      viewBox='0 0 21 21'
-      width='21'
-      xmlns='http://www.w3.org/2000/svg'
+  <svg
+    className='inline fill-current'
+    height='21'
+    viewBox='0 0 21 21'
+    width='21'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <g
+      fill='none'
+      fill-rule='evenodd'
+      stroke='var(--text-body)'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      transform='translate(5 6)'
     >
-      <g
-        fill='none'
-        fill-rule='evenodd'
-        stroke='var(--text-body)'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        transform='translate(5 6)'
-      >
-        <path d='m8.5 8.5-4-4 4-4' />
-        <path d='m4.5 8.5-4-4 4-4' />
-      </g>
-    </svg>
-  </ChevronWrapper>
+      <path d='m8.5 8.5-4-4 4-4' />
+      <path d='m4.5 8.5-4-4 4-4' />
+    </g>
+  </svg>
 )
 
 export const DoubleChevronRight = () => (
-  <ChevronWrapper>
-    <svg
-      className='inline fill-current'
-      height='21'
-      viewBox='0 0 21 21'
-      width='21'
-      xmlns='http://www.w3.org/2000/svg'
+  <svg
+    className='inline fill-current'
+    height='21'
+    viewBox='0 0 21 21'
+    width='21'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <g
+      fill='none'
+      fill-rule='evenodd'
+      stroke='var(--text-body)'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      transform='translate(7 6)'
     >
-      <g
-        fill='none'
-        fill-rule='evenodd'
-        stroke='var(--text-body)'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        transform='translate(7 6)'
-      >
-        <path d='m.5 8.5 4-4-4-4' />
-        <path d='m4.5 8.5 4-4-4-4' />
-      </g>
-    </svg>
-  </ChevronWrapper>
+      <path d='m.5 8.5 4-4-4-4' />
+      <path d='m4.5 8.5 4-4-4-4' />
+    </g>
+  </svg>
 )
