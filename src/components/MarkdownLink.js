e3b5bfcbeb88b1b95ef369956e40c4578b6fa985
Ken Feliciano <kafeliciano@gmail.com>
2022-02-14 15:40:28 -0800

    Use `ExternalLink` in `MarkdownLink` component
    I have the exact same code in `MarkdownLink` as `ExternalLink` so I'm
    switching to using that instead of this duplicate code.
    After all this, I'm not using GatsbyLink anymore so that goes away.

--- src/components/MarkdownLink.js
+++ src/components/MarkdownLink.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
-import { Link as GatsbyLink } from 'gatsby'
-import { InternalLink } from '.'
+import { InternalLink, ExternalLink } from '.'
 
 // Checks against absolute URLs that share 👇 so we can still pass it along to Gatsby's internal link component
 const domainRegex = /http[s]*:\/\/[www.]*koamar\.com[/]?/
@@ -30,16 +29,7 @@ export const MarkdownLink = ({ href, ...rest }) => {
     return <a href={href} {...rest} /> // eslint-disable-line jsx-a11y/anchor-has-content
   }
 
-  return (
-    <a
-      data-link-external
-      href={href}
-      className='inline-link border-fadeaway'
-      target='_blank'
-      rel='noopener noreferrer nofollow'
-      {...rest}
-    />
-  )
+  return <ExternalLink href={href} {...rest} />
 }
 
 MarkdownLink.propTypes = {
