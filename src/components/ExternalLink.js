4db00ab985b5efc78ea11b70570b24bbde7d5f64
Ken Feliciano <kafeliciano@gmail.com>
2022-02-14 16:23:02 -0800

    Add simple arrow SVG component for external links
    Nice as the CSS idea was, this works in dark mode.
    Changing background so it is apparent when it is hovered/focused on.

--- src/components/ExternalLink.js
+++ src/components/ExternalLink.js
@@ -1,12 +1,14 @@
 import * as React from 'react'
+import { ExternalLinkIcon } from '../svgs'
 
 export const ExternalLink = ({ href, children }) => (
   <a
     href={href}
-    className='inline-link external-link border-fadeaway'
+    className='inline-link external-link border-fadeaway hover:bg-brandedSurface focus:bg-brandedSurface'
     target='_blank'
     rel='noreferrer noopener nofollow'
   >
     {children}
+    <ExternalLinkIcon />
   </a>
 )
