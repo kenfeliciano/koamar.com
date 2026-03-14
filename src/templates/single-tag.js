9a72bb8e442ca86ac5f3d464047a468f1a82018c
Ken Feliciano <kafeliciano@gmail.com>
2026-03-14 14:38:09 -0700

    Set Head in tag-list
    Now it displays #tag-name instead of unedefined!

--- src/templates/single-tag.js
+++ src/templates/single-tag.js
@@ -2,12 +2,16 @@ import * as React from 'react'
 import SEO from '../components/seo'
 import { Layout, Content, PostList } from '../components'
 
+export const Head = ({ pageContext }) => {
+  const { id } = pageContext
+  return <SEO title={`#${id}`} />
+}
+
 const tagPosts = ({ pageContext }) => {
   const { id, posts } = pageContext
 
   return (
     <Layout>
-      <SEO />
       <Content>
         <h1>{id}</h1>
         <PostList posts={posts} />
