62620eb9971dbb7e628bd02da6fb43ed0a59c1b3
Ken Feliciano <kafeliciano@gmail.com>
2026-03-24 22:52:33 -0700

    Use StyleSheetManager to avoid hard refresh in development
    - Add wrapRootElement to gatsby-ssr and add StyleSheetManager
    - Move onRenderBody from gatsby-wrapper
    - Add a div to the rendered HTML before Gatsby content for the style sheet
    - Add StyleSheetManager to gatsby-browser.
    - Use the div created in gatsby-ssr as the target for the style sheet

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,14 @@
+import * as React from 'react'
+import { StyleSheetManager } from 'styled-components'
+import { wrapRootElement as wrap } from './gatsby-wrapper'
+
 import './src/styles/global.css'
 import './src/styles/code-layout.css'
-export { wrapRootElement } from './gatsby-wrapper'
+
+export const wrapRootElement = ({ element }) => {
+  if (typeof document !== 'undefined') {
+    const target = document.getElementById('sc-target')
+    return <StyleSheetManager target={target}>{wrap({ element })}</StyleSheetManager>
+  }
+  return wrap({ element })
+}
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,1 +1,14 @@
-export { wrapRootElement, onRenderBody } from './gatsby-wrapper'
+import * as React from 'react'
+import { StyleSheetManager } from 'styled-components'
+import { wrapRootElement as wrap, MagicScriptTag } from './gatsby-wrapper'
+
+export const wrapRootElement = ({ element }) => {
+  return <StyleSheetManager target='#sc-target'>{wrap({ element })}</StyleSheetManager>
+}
+
+export const onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
+  // MagicScriptTag MUST run before hydration
+  setHeadComponents([<MagicScriptTag key='magic-script' />])
+
+  setPreBodyComponents([<div id='sc-target' key='sc-target'></div>])
+}
